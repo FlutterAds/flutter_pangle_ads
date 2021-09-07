de6c08929102ca182563a17bde8734b98a2d6cd5
Zero <1300326388@qq.com>
2021-09-07 14:35:27 +0800

    1、优化修复开屏 Logo 去不掉的问题

--- ios/Classes/Page/SplashPage.m
+++ ios/Classes/Page/SplashPage.m
@@ -16,15 +16,15 @@
     self.fullScreenAd=[logo isKindOfClass:[NSNull class]]||[logo length]==0;
     // 计算大小
     CGFloat adHeight=self.height;// 广告区域的高度
+    self.bottomView=[[UIView alloc]initWithFrame:CGRectMake(0, 0,self.width, self.height)];
+    self.bottomView.backgroundColor=[UIColor whiteColor];
     // 非全屏设置 Logo
     if(!self.fullScreenAd){
         CGFloat logoHeight=112.5;// 这里按照 15% 进行logo 的展示，防止尺寸不够的问题，750*15%=112.5
         adHeight=self.height-logoHeight;// 广告区域的高度
         // 设置底部 logo
-        self.bottomView=[[UIView alloc]initWithFrame:CGRectMake(0, adHeight,self.width, logoHeight)];
-        self.bottomView.backgroundColor=[UIColor whiteColor];
         UIImageView *logoView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:logo]];
-        logoView.frame=CGRectMake(0, 0, self.width, logoHeight);
+        logoView.frame=CGRectMake(0, adHeight, self.width, logoHeight);
         logoView.contentMode=UIViewContentModeCenter;
         [self.bottomView addSubview:logoView];
     }
@@ -36,16 +36,15 @@
     // 加载全屏广告
     [self.splashAd loadAdData];
     // 设置广告 View
-    [self.mainWin.rootViewController.view addSubview:self.splashAd];
+    [self.bottomView addSubview:self.splashAd];
+//    [self.mainWin.rootViewController.view addSubview:self.splashAd];
+    [self.mainWin.rootViewController.view addSubview:self.bottomView];
     self.splashAd.rootViewController=self.mainWin.rootViewController;
 }
 
 // 删除广告 View
 - (void)removeSplashAdView {
-    if (self.splashAd) {
-        [self.splashAd removeFromSuperview];
-        self.splashAd = nil;
-    }
+    NSLog(@"%s",__FUNCTION__);
     // 底部 Logo
     if(self.bottomView){
         [self.bottomView removeFromSuperview];
@@ -57,7 +56,6 @@
 
 - (void)splashAdDidLoad:(BUSplashAdView *)splashAd {
     NSLog(@"%s",__FUNCTION__);
-    [self.mainWin.rootViewController.view addSubview:self.bottomView];
     // 发送广告事件
     [self sendEventAction:onAdLoaded];
     
@@ -65,23 +63,19 @@
 
 - (void)splashAdDidClose:(BUSplashAdView *)splashAd {
     NSLog(@"%s",__FUNCTION__);
-    [splashAd removeFromSuperview];
+    [self removeSplashAdView];
     // 发送广告事件
     [self sendEventAction:onAdClosed];
 }
 
 - (void)splashAdDidClick:(BUSplashAdView *)splashAd {
     NSLog(@"%s",__FUNCTION__);
-    // Be careful not to say 'self.splashadview = nil' here.
-    // Subsequent agent callbacks will not be triggered after the 'splashAdView' is released early.
-    [splashAd removeFromSuperview];
     // 发送广告事件
     [self sendEventAction:onAdClicked];
 }
 
 - (void)splashAdDidClickSkip:(BUSplashAdView *)splashAd {
     NSLog(@"%s",__FUNCTION__);
-    [self removeSplashAdView];
     // 发送广告事件
     [self sendEventAction:onAdSkip];
 }
@@ -99,9 +93,8 @@
     [self sendEventAction:onAdExposure];
 }
 
-- (void)splashAdDidCloseOtherController:(BUSplashAdView *)splashAd interactionType:(BUInteractionType)interactionType {
+- (void)splashAdDidCloseOtherController:(BUSplashAdView *)splashAd interactionType:(BUInteractionType)interactionType{
     NSLog(@"%s",__FUNCTION__);
-    [self removeSplashAdView];
 }
 
 - (void)splashAdCountdownToZero:(BUSplashAdView *)splashAd {
